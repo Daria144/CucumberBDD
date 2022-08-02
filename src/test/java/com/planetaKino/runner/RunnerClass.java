a4120d8aee942a8773034dba4d7890446a6b9438
dyatsenko39532 <daria.yatsenko@cengage.com>
2022-08-02 16:29:15 +0300

    add html report

--- src/test/java/com/planetaKino/runner/RunnerClass.java
+++ src/test/java/com/planetaKino/runner/RunnerClass.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/resources/features/movies.feature",
         glue = {"com/planetaKino/steps","com/planetaKino/utils"},
-        plugin = {"pretty"},
+        plugin = {"pretty","html:target/cucumber-reports"},
         monochrome = true)
 public class RunnerClass extends AbstractTestNGCucumberTests {
 }
